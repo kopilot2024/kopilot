644dd864c2fa786102d02abc78177b41351019c1
ttobe <ahdrmfgur12@gmail.com>
2024-07-10 11:05:57 +0900

    fix: spellCheck 파일 분리 및 '로 수정 #6

---
+++ hanspell-server/views/spellCheck.js
@@ -0,0 +1,17 @@
+﻿async function spellCheck(sentence) {
+  const URL = 'http://localhost:3000/';
+  try {
+    const response = await fetch(URL, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      body: new URLSearchParams({sentence}),
+    });
+
+    return await response.json();
+  } catch (error) {
+    console.error('Error during spell check:', error);
+    throw error;
+  }
+}
