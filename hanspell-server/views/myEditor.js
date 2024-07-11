edb97863277083fa3b3fc84f34c50c0aefa70a12
ttobe <ahdrmfgur12@gmail.com>
2024-07-11 22:27:35 +0900

    refactor: utils와 myEditor로 함수 분리 및 안쓰는 변수 제거 #6

---
+++ hanspell-server/views/myEditor.js
@@ -0,0 +1,43 @@
+﻿// block api 사용 클래스
+class MyTool {
+  constructor({api}) {
+    this.api = api;
+  }
+
+  // 렌더링된 블록의 개수를 가져오는 메서드
+  getBlocksCount() {
+    return this.api.blocks.getBlocksCount();
+  }
+
+  // 현재 포커스된 블록의 인덱스를 가져오는 메서드
+  getCurrentBlockIndex() {
+    return this.api.blocks.getCurrentBlockIndex();
+  }
+
+  // 블럭을 인덱스로 가져오기
+  getBlockByIndex(index) {
+    return this.api.blocks.getBlockByIndex(index);
+  }
+}
+
+/**
+ * api 사용 인스턴스
+ */
+let myToolInstance;
+
+// EditorJS 초기화 및 MyTool 등록
+const editor = new EditorJS({
+  holder: 'editorjs',
+  tools: {
+    myTool: {
+      class: MyTool,
+    },
+  },
+  onReady: () => {
+    myToolInstance = new MyTool({api: editor});
+    addGlobalEventListeners()
+  },
+  onChange: () => {
+    //TODO: 블럭 삭제 시 index 초기화 잘 해야함.
+  },
+});
---
+++ hanspell-server/views/utils.js
@@ -0,0 +1,21 @@
+﻿/**
+ * 특수문자 처리
+ * @param string
+ */
+function escapeRegExp(string) {
+  return string.replace(/[.*+?^${}()|[\]\\]/g, '\\$&'); // $&는 매치된 전체 문자열을 의미합니다.
+}
+
+/**
+ * span 제거 함수
+ * @param content
+ */
+function removeSpan(content) {
+  return content.replace(/<span[^>]*>|<\/span>/g, '');
+}
+
+/**
+ * 문장부호 . ? !
+ * @type {string[]}
+ */
+const marks = ['.', '?', '!'];
