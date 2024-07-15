7ab559f088196b1a61a50c914ec6198c38309c71
kimyu0218 <serena35@ewhain.net>
2024-07-15 18:18:35 +0900

    init: 프로젝트 생성

---
+++ src/app.controller.ts
@@ -0,0 +1,13 @@
+import { Controller, Get, Render, Res } from '@nestjs/common';
+import { AppService } from './app.service';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  @Render('index')
+  root(): string {
+    return;
+  }
+}
---
+++ src/main.ts
@@ -0,0 +1,15 @@
+import { join } from 'path';
+import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  app.useStaticAssets(join(__dirname, '..', 'views'));
+  app.setBaseViewsDir(join(__dirname, '..', 'views'));
+  app.setViewEngine('hbs');
+
+  await app.listen(3000);
+}
+bootstrap();
