d9d0a687d5ff969b4a5ce36873fa7274a00c5515
kimyu0218 <serena35@ewhain.net>
2024-07-16 17:04:21 +0900

    feat: index.hbs에 사용되는 js 파일 작성 [#1 #3]

---
+++ views/javascript/index.js
@@ -0,0 +1,21 @@
+import { AutoCompleteSettings } from './autoCompleteSettings.js';
+import { CursorBox } from './components/cursorBox.js';
+import { EditTextarea } from './editTextarea.js';
+
+const textarea = document.getElementById('edit-textarea');
+
+const cursorBox = new CursorBox(
+  document.getElementById('cursor-box'),
+  textarea,
+);
+
+const autoCompleteSettings = new AutoCompleteSettings(cursorBox);
+
+const editTextarea = new EditTextarea(textarea, autoCompleteSettings);
+
+const endingChoice = document.getElementById('ending-choice');
+endingChoice.addEventListener('change', handleRadioBtnChangeEvent);
+
+function handleRadioBtnChangeEvent(event) {
+  editTextarea.changeEndingType(event.target.value);
+}
