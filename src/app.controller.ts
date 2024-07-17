dedbb6f76b6d413c286b443e4cd1554157b2350c
ttobe <ahdrmfgur12@gmail.com>
2024-07-17 15:28:52 +0900

    fix: merge conflict 해결 #6

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,7 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Render } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
+
+  @Get()
+  @Render('auto-complete')
+  root(): string {
+    return;
+  }
 }
