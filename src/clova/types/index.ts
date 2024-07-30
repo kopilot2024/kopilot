0c307cc203a8da18a823ad5f0667b8c8ef69219e
kimyu0218 <serena35@ewhain.net>
2024-07-30 16:58:37 +0900

    feat: 추가적인 상수 export

--- src/clova/types/index.ts
+++ src/clova/types/index.ts
@@ -1,4 +1,5 @@
 export * from './clova-chat-completions';
 export * from './clova-completions';
 export * from './clova-request-header.type';
+export * from './partial-modification';
 export * from './partial-response.type';
