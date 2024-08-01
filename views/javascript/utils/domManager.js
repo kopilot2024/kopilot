23608dd97edebb6dade4a9e64261d0e86fec7ea8
kimyu0218 <serena35@ewhain.net>
2024-08-01 17:02:49 +0900

    feat: 싱크를 위한 메서드 추가 [#55]

--- views/javascript/utils/domManager.js
+++ views/javascript/utils/domManager.js
@@ -1,11 +1,20 @@
 import { STYLE } from '../constants/style.js';
 
 export class DomManager {
-  static showElement(child, display = STYLE.DISPLAY.FLEX) {
-    child.style.display = display;
+  static showElement(element, display = STYLE.DISPLAY.FLEX) {
+    element.style.display = display;
   }
 
-  static hideElement(child, display = STYLE.DISPLAY.NONE) {
-    child.style.display = display;
+  static hideElement(element, display = STYLE.DISPLAY.NONE) {
+    element.style.display = display;
+  }
+
+  static syncElements(subject, object) {
+    object.scrollTop = subject.scrollTop;
+    object.scrollLeft = subject.scrollLeft;
+  }
+
+  static changeVisibility(element, visibility) {
+    element.style.visibility = visibility;
   }
 }
