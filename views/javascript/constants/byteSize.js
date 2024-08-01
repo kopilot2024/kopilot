0731525aa933ecc307c56b98e459c3982f569545
ttobe <ahdrmfgur12@gmail.com>
2024-08-01 11:18:09 +0900

    refactor: countChar 분리 및 const 설정

---
+++ views/javascript/constants/byteSize.js
@@ -0,0 +1,5 @@
+export const BYTE_SIZE = {
+  BYTE_SIZE_1: 1,
+  BYTE_SIZE_2: 2,
+  BYTE_SIZE_3: 3,
+};
---
+++ views/javascript/countChar/countChar.js
@@ -0,0 +1,25 @@
+import { BYTE_SIZE } from '../constants/byteSize.js';
+
+export class CountChar {
+  static countChar(element) {
+    const charCount = document.getElementById('charCount');
+    charCount.textContent = `공백 포함: ${element.value.length}자, Byte 수: ${this.#getByteLength(element.value)}`;
+  }
+
+  static #getByteLength(str) {
+    let byteSize = 0;
+
+    for (let i = 0; i < str.length; i++) {
+      const charCode = str.charCodeAt(i);
+      if (charCode >= 2048) {
+        byteSize += BYTE_SIZE.BYTE_SIZE_3;
+      } else if (charCode >= 128) {
+        byteSize += BYTE_SIZE.BYTE_SIZE_2;
+      } else {
+        byteSize += BYTE_SIZE.BYTE_SIZE_1;
+      }
+    }
+
+    return byteSize;
+  }
+}
