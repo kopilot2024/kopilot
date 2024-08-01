5a44c2d50376668aeff28b94e23aa15d56bfd9d1
kimyu0218 <serena35@ewhain.net>
2024-08-01 13:17:14 +0900

    feat: event key를 위한 상수 별도 정의

---
+++ views/javascript/constants/eventKey.js
@@ -0,0 +1,16 @@
+export const KEY = {
+  ENTER: 'Enter',
+  SPACE: 'Space',
+  TAB: 'Tab',
+
+  ARROW_UP: 'ArrowUp',
+  ARROW_DOWN: 'ArrowDown',
+  ARROW_LEFT: 'ArrowLeft',
+  ARROW_RIGHT: 'ArrowRight',
+
+  BACKSPACE: 'Backspace',
+
+  PERIOD: '.',
+  QUESTION_MARK: '?',
+  EXCLAMATION_MARK: '!',
+};
